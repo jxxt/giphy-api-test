8ab74f652105231012a06004b696f85946955ed4
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-14 12:05:12 +0530

    added features

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const imageDiv = document.querySelector(".image");
 
+const categoryInput = document.querySelector(".category");
+const submit = document.querySelector(".submit");
+
+const newBtn = document.querySelector(".new");
+
 const API_KEY = "bb2006d9d3454578be1a99cfad65913d";
 
-let category = "dogs";
+const loadGif = (category) => {
+    fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${category}`,
+        { mode: "cors" }
+    )
+        .then(function (response) {
+            return response.json();
+        })
 
-fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${category}`,
-    { mode: "cors" }
-)
-    .then(function (response) {
-        return response.json();
-    })
+        .then(function (response) {
+            let url = response.data.images.original.url;
+            console.log(url);
+            imageDiv.innerHTML = `<img src="${url}" alt="">`;
+        });
+};
 
-    .then(function (response) {
-        let url = response.data.images.original.url;
-        imageDiv.innerHTML = `<img src="${url}" alt="">`;
-    });
+submit.addEventListener("click", () => {
+    let category = categoryInput.value;
+    console.log(category);
+
+    loadGif(category);
+});
+
+//
+
+newBtn.addEventListener("click", () => {
+    let category = categoryInput.value;
+    console.log(category);
+
+    loadGif(category);
+});
