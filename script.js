529e11d5b264d7d3865dd4e14bc17f0b3e96483f
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-14 11:32:58 +0530

    added api gif

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const imageTag = document.querySelector(".image-tag")
+const imageDiv = document.querySelector(".image");
 
-const API_KEY = "bb2006d9d3454578be1a99cfad65913d"
+const API_KEY = "bb2006d9d3454578be1a99cfad65913d";
 
-let category = "dogs"
+let category = "dogs";
 
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${category}`, { mode: 'cors' })
+fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${category}`,
+    { mode: "cors" }
+)
+    .then(function (response) {
+        return response.json();
+    })
 
-.then(function (response) {
-    return response.json();
-})
-
-.then(function (response) {
-    console.log(response);
-});
-
+    .then(function (response) {
+        let url = response.data.images.original.url;
+        imageDiv.innerHTML = `<img src="${url}" alt="">`;
+    });
