cc6760860aa61abd3bc9535a0199d166ac609937
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-14 16:50:40 +0530

    added async await js code

--- script.js
+++ script.js
@@ -7,34 +7,45 @@ const newBtn = document.querySelector(".new");
 
 const API_KEY = "bb2006d9d3454578be1a99cfad65913d";
 
-const loadGif = (category) => {
+const promisesLoadGif = (category) => {
     fetch(
         `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${category}`,
         { mode: "cors" }
     )
-        .then(function (response) {
+        .then((response) => {
             return response.json();
         })
 
-        .then(function (response) {
+        .then((response) => {
             let url = response.data.images.original.url;
             console.log(url);
             imageDiv.innerHTML = `<img src="${url}" alt="">`;
         });
 };
 
+const asyncLoadGif = async (category) => {
+    const response = await fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${category}`,
+        { mode: "cors" }
+    );
+
+    let responseJson = await response.json();
+
+    let url = responseJson.data.images.original.url;
+    console.log(url);
+    imageDiv.innerHTML = `<img src="${url}" alt="">`;
+};
+
 submit.addEventListener("click", () => {
     let category = categoryInput.value;
     console.log(category);
 
-    loadGif(category);
+    asyncLoadGif(category);
 });
-
-//
 
 newBtn.addEventListener("click", () => {
     let category = categoryInput.value;
     console.log(category);
 
-    loadGif(category);
+    asyncLoadGif(category);
 });
